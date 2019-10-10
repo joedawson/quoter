1160042a0fd993d4c062989e1617905489dc90a1
Joe Dawson <j.dawson@impactmedia.co.uk>
2019-10-10 15:35:51 +0100

    Some kind of working PDF generator

--- listeners/GeneratePDF.php
+++ listeners/GeneratePDF.php
@@ -2,12 +2,41 @@
 
 namespace App\Listeners;
 
+use Dompdf\Dompdf;
 use TightenCo\Jigsaw\Jigsaw;
 
 class GeneratePDF
 {
+    protected $exclude = [
+        '.', '..', 'assets'
+    ];
+
     public function handle(Jigsaw $jigsaw)
     {
-        // generate the PDF...
+        $destination = $jigsaw->getDestinationPath();
+
+        $pages = collect(scandir($destination))->reject(function($path) {
+            return $this->isExcluded($path);
+        });
+
+        if($pages->count())
+        {
+            $pdf = new Dompdf();
+
+            $pages->each(function($page) use ($pdf, $destination) {
+                return $pdf->loadHtml(file_get_contents($destination . '/' . $page));
+            });
+
+            $pdf->render();
+
+            $output = $pdf->output();
+
+            file_put_contents($destination . '/' . 'file.pdf', $output);
+        }
+    }
+
+    public function isExcluded($path)
+    {
+        return str_is($this->exclude, $path);
     }
 }
