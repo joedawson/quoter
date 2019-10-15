1dc70fde08c8292c448e7febf238dd7b2379c313
Joe Dawson <j.dawson@impactmedia.co.uk>
2019-10-15 17:03:43 +0100

    Page break utilities
    Will eventually turn this into a plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,22 @@ module.exports = {
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [
+    function({ addUtilities }) {
+      const pageBreakUtilities = {
+        '.pbb-auto': { 'page-break-before': 'auto', },
+        '.pbb-always': { 'page-break-before': 'always', },
+        '.pbb-avoid': { 'page-break-before': 'avoid', },
+        '.pbb-left': { 'page-break-before': 'left', },
+        '.pbb-right': { 'page-break-before': 'right', },
+        '.pba-auto': { 'page-break-after': 'auto', },
+        '.pba-always': { 'page-break-after': 'always', },
+        '.pba-avoid': { 'page-break-after': 'avoid', },
+        '.pba-left': { 'page-break-after': 'left', },
+        '.pba-right': { 'page-break-after': 'right', },
+      }
+
+      addUtilities(pageBreakUtilities);
+    }
+  ]
 }
