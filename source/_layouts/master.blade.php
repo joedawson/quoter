0046dede0e7bda82bf3a6bd52dd595be115ab2f3
Joe Dawson <j.dawson@impactmedia.co.uk>
2019-10-15 16:31:42 +0100

    Cover setup

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -8,6 +8,14 @@
         <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/build') }}">
     </head>
     <body class="font-sans antialiased">
+        @hasSection('cover')
+            <header class="bg-blue-500 text-white pt-12 pb-6 px-8 h-screen">
+                <div class="max-w-2xl mx-auto">
+                    @yield('cover')
+                </div>
+            </header>
+        @endif
+
         @hasSection('header')
             <header class="bg-blue-500 text-white pt-12 pb-6 px-8">
                 <div class="max-w-2xl mx-auto">
@@ -21,11 +29,5 @@
                 @yield('body')
             </div>
         </div>
-
-        <footer class="px-8">
-            <div class="max-w-2xl mx-auto border-t py-6">
-                <a href="https://joedawson.me">joedawson.me</a>
-            </div>
-        </footer>
     </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,5 @@
 @extends('_layouts.master')
 
-@section('header')
+@section('cover')
     <h1 class="text-2xl">Cover</h1>
-@endsection
-
-@section('body')
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet. Sed sit amet arcu aliquet, molestie justo at, auctor nunc. Phasellus ligula ipsum, volutpat eget semper id, viverra eget nibh. Suspendisse luctus mattis cursus. Nam consectetur ante at nisl hendrerit gravida.</p>
-
 @endsection
