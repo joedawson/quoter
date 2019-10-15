33a6406b5fd2bdb6ef7065b419759f75958cd48c
Joe Dawson <j.dawson@impactmedia.co.uk>
2019-10-15 17:04:19 +0100

    Additional page to help test page breaks

---
+++ source/spec.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.master')
+
+@section('header')
+    <h1 class="text-2xl">Specification</h1>
+@endsection
+
+@section('body')
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet. Sed sit amet arcu aliquet, molestie justo at, auctor nunc. Phasellus ligula ipsum, volutpat eget semper id, viverra eget nibh. Suspendisse luctus mattis cursus. Nam consectetur ante at nisl hendrerit gravida.</p>
+
+    <p>Donec vehicula rhoncus mattis. Mauris dignissim semper mattis. Fusce porttitor a mi at suscipit. Praesent facilisis dolor sapien, vel sodales augue mollis ut. Mauris venenatis magna eu tortor posuere luctus. Aenean tincidunt turpis sed dui aliquam vehicula. Praesent nec elit non dolor consectetur tincidunt sed in felis. Donec elementum, lacus at mattis tincidunt, eros magna faucibus sem, in condimentum est augue tristique risus.</p>
+
+@endsection
